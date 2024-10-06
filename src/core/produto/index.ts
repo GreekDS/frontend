d725f138a1cadf0b7fcd5901fbda0704c459f87f
GreekDS <ds423323@gmail.com>
2024-10-06 16:50:15 -0300

    Adicionar exportações e constantes do módulo principal

---
+++ src/core/produto/index.ts
@@ -0,0 +1,5 @@
+import Especificacoes from "./Especificacoes";
+import Produto from "./Produto";
+import Precificavel from "./Precificavel";
+
+export type { Especificacoes, Produto, Precificavel };
